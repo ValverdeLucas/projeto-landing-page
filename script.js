e7c454e5b60471593b5b3dc9d6ebd3ea535c20e1
Lucas Valverde <valverdelucas95@gmail.com>
2023-10-22 22:12:57 -0300

    Finalização Projeto - HTML / CSS / JS

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function contato(event) {
         retornoFormulario.style.fontWeight = "bold";
         retornoFormulario.setAttribute('id', 'retorno')
         retornoFormulario.setAttribute('class', 'retorno-negativo')
-        
+
         const formularioIncompleto = document.createTextNode("Ops! Você deixou um ou mais campo vazio, corrija e envie novamente!")
         retornoFormulario.appendChild(formularioIncompleto)
 
@@ -23,10 +23,11 @@ function contato(event) {
 
     } else {
 
-        
+
         respostaFormulario.remove()
 
         const local = document.getElementById("contato")
+
         const retornoFormulario = document.createElement("p")
         retornoFormulario.style.fontWeight = "bold";
         retornoFormulario.setAttribute('id', 'retorno')
@@ -39,3 +40,43 @@ function contato(event) {
 
     }
 }
+
+
+function fecharPedido(event) {
+
+    event.preventDefault()
+
+    const localRevisao = document.getElementById("caixa-pagamento");
+    localRevisao.style.display = 'block';
+
+    const listaRevisao = document.getElementById("lista-revisao");
+
+    const itensAdicionados = document.createElement("li");
+    const itemTeste = document.createTextNode('Item teste');
+    itensAdicionados.appendChild(itemTeste);
+
+    listaRevisao.insertAdjacentElement('beforeend', itensAdicionados);
+
+}
+
+
+function confirmarPedido(event) {
+
+    event.preventDefault()
+
+    const local = document.getElementById("caixa-pagamento")
+
+    const confirmacaoPedido = document.createElement("p")
+    confirmacaoPedido.style.fontWeight = "bold";
+    confirmacaoPedido.setAttribute('id', 'revisao')
+    confirmacaoPedido.setAttribute('class', 'retorno-positivo')
+
+    const respostaConfirmacao = document.createTextNode("Pedido confirmado!")
+    confirmacaoPedido.appendChild(respostaConfirmacao)
+    
+    local.insertAdjacentElement('beforeend', confirmacaoPedido)
+
+}
+
+
+
